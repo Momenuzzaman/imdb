1a8c608485e251e902a1d5c3351dea67c2061a52
mdemon420 <mdeamon4@gmail.com>
2023-08-30 14:51:06 +0600

    drak mode switch create and implement.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* header */}
-        <Header />
-        {children}
+        <Providers>
+          {/* header */}
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import MenuItems from "./MenuItems";
 import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
+import DarkModeSwitch from "./DarkModeSwitch";
 const Header = () => {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+    <div className="flex justify-between mx-5 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItems title="Home" address="/" Icon={AiFillHome} />
         <MenuItems title="About" address="/about" Icon={AiFillInfoCircle} />
       </div>
-      <div>
+      <div className="flex items-center space-x-5 md:mx-1">
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg ">
